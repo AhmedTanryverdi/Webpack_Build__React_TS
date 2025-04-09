b81fd59683afd39776af927919593a4bc80d19d8
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-09 17:09:26 +0300

    config app

--- src/app/entry/main.tsx
+++ src/app/entry/main.tsx
@@ -1,7 +1,8 @@
-import { createRoot } from "react-dom/client";
-
 import React from "react";
+import { createRoot } from "react-dom/client";
 import { App } from "./ui/App";
+import './styles/app.css'
+
 
 createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
