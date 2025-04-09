6cb8bc93cfd9bc3b159a0c978124e49de1f45668
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-10 01:49:10 +0300

    fix

--- src/widgets/header/ui/navigate/Navigate.tsx
+++ src/widgets/header/ui/navigate/Navigate.tsx
@@ -9,7 +9,7 @@ export const Navigate: React.FC = (): React.JSX.Element => {
 		<div className="navigate">
 			{navigateMenu.map((item: string, index: number) => {
 				if(index >=1 && index <= 2){
-					return <button type="button">{item}</button>;
+					return <button key={index} type="button">{item}</button>;
 				}
 				return (
 					<Link key={index} to={index === 3 ? "/about" : index === 4 ? "/contacts": '/'}>
