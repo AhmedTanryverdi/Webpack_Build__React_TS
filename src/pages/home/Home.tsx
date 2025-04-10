8e0d6a4e32e8fa6ff936f38ec79c80443577b58b
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-10 18:49:38 +0300

    added section products in home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Main } from "./ui/main/Main";
 import { SneakersModels } from "./ui/sneakers-models/SneakersModels";
 import "./home.scss";
+import { ProductCard } from "@/shared/components/product-card/ProductCard";
 
 export const Home: React.FC = (): React.JSX.Element => {
 	return (
@@ -9,6 +10,28 @@ export const Home: React.FC = (): React.JSX.Element => {
 			<div className="container">
 				<Main />
 				<SneakersModels />
+
+				<div className="products">
+					<h2 className="products__title">
+						<p className="title">New Products Arrival</p>
+						<p className="subtitle">
+							Duis vestibulum elit vel neque pharetra
+						</p>
+					</h2>
+
+					<div className="products__block">
+						{Array.from({ length: 4 }).map((item, index) => {
+							return (
+								<ProductCard
+									key={index}
+									title="title"
+									price="$45"
+									img="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+								/>
+							);
+						})}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
