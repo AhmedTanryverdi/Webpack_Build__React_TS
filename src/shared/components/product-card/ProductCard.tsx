a9e556ef13ff2f9d4ee2b3432b463f314f5bf92f
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-12 09:25:06 +0300

    fix home page

--- src/shared/components/product-card/ProductCard.tsx
+++ src/shared/components/product-card/ProductCard.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import './productcard.scss';
 import { Button } from '../button/Button';
+import "./productcard.scss";
+
+
 
 export const ProductCard: React.FC<{ title: string; price: string; img: string }> = ({
 	title,
 	price,
 	img,
 }): React.JSX.Element => {
+
 	return (
 		<div className="product-card">
 			<img src={img} alt="photo" className="product-card__img" />
 			<h3 className="product-card__title">
-				{title}
-				<p className="product-card__price">{price}</p>
+				<p className="title">{title}</p>
+				<p className="price">${price}</p>
 			</h3>
+
 			<Button text="View Product" name="product-card__btn" />
 		</div>
 	);
