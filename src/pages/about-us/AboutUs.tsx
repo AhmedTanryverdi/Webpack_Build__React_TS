678b42e7eb14823902924221176a16fa3fb78c24
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-12 14:12:56 +0300

    added the section sub-promo in the about-us page

--- src/pages/about-us/AboutUs.tsx
+++ src/pages/about-us/AboutUs.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Promo } from "./ui/promo/Promo";
+import { SubPromo } from "./ui/subpromo/SubPromo";
 
 export const AboutUs: React.FC = (): React.JSX.Element => {
 	return (
 		<div className="about-us">
 			<div className="container">
         <Promo />
+        <SubPromo />
       </div>
 		</div>
 	);
