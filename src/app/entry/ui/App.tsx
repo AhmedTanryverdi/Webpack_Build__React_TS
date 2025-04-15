e367264774d04f0900174c655875ebdf55545419
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-15 20:49:32 +0300

    added products path

--- src/app/entry/ui/App.tsx
+++ src/app/entry/ui/App.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { Root } from "./Root";
-import {Home} from '@/pages/home/Home';
+import { Home } from "@/pages/home/Home";
 import { AboutUs } from "@/pages/about-us/AboutUs";
 import { ContactUs } from "@/pages/contact-us/ContactUs";
-
+import { Products } from "@/pages/products/Products";
 
 export const App: React.FC = (): React.JSX.Element => {
 	return (
@@ -13,6 +13,7 @@ export const App: React.FC = (): React.JSX.Element => {
 					<Route index element={<Home />} />
 					<Route path="about" element={<AboutUs />} />
 					<Route path="contacts" element={<ContactUs />} />
+					<Route path="products" element={<Products />} />
 				</Route>
 			</Routes>
 		</div>
