72c61e622a02b51041d4c7b8a6578b33a6c2cdf8
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-20 13:03:39 +0300

    added component

---
+++ src/widgets/header/ui/direct/ui/Access.tsx
@@ -0,0 +1,11 @@
+import "./access.scss";
+import React from 'react';
+
+export const Access: React.FC = (): React.JSX.Element => {
+  return (
+    <div className="access">
+        <div className="login">Вход</div>
+        <div className="register">Регистрация</div>
+    </div>
+  )
+}
