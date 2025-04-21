c7710a63cee712d2a008deab4dcad07ff0e09771
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-21 16:11:17 +0300

    added logic to direct block

--- src/widgets/header/ui/direct/Direct.tsx
+++ src/widgets/header/ui/direct/Direct.tsx
@@ -1,21 +1,78 @@
 import "./direct.scss";
-import { images } from "./utils/constants/constants";
-import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import { Access } from "./ui/Access";
+import React, { useEffect, useState } from "react";
 
 export const Direct: React.FC = (): React.JSX.Element => {
+	const [isAccess, setAccess] = useState(false);
+	const [isSearch, setSearch] = useState(false);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const fn = (e: any) => {
+			if (e.target instanceof HTMLElement) {
+				if (e.target.parentElement?.className !== "direct") {
+					setSearch(false);
+				}
+
+				if (e.target.parentElement?.className !== "direct__user") {
+					setAccess(false);
+				} else {
+				}
+			}
+		};
+
+		document.addEventListener("click", fn);
+
+		return () => document.removeEventListener("click", fn);
+	}, []);
+
 	return (
 		<div className="direct">
-			{images.map((image, index) => {
-				return (
-					<img
-						key={index}
-						src={require(`@/widgets/header/assets/icons/${image.title}.png`).default}
-						alt="icon"
-						className="header__icons_item"
-					/>
-				);
-			})}
+			<div className="direct__input">
+				<input
+					type="text"
+					className={`input ${isSearch && "active"}`}
+				/>
+			</div>
+			<img
+				src={
+					require(`@/widgets/header/assets/icons/search.png`).default
+				}
+				alt="search image"
+				className="search"
+				onClick={() => {
+					setAccess(false);
+					setSearch(!isSearch);
+				}}
+			/>
+			<div className="direct__user">
+				<img
+					src={
+						require(`@/widgets/header/assets/icons/user.png`)
+							.default
+					}
+					alt="user image"
+					onClick={() => {
+						setSearch(false);
+						setAccess(!isAccess);
+					}}
+				/>
+				{isAccess && <Access />}
+			</div>
+
+			<img
+				src={
+					require(`@/widgets/header/assets/icons/shopping-cart.png`)
+						.default
+				}
+				alt="cart image"
+				onClick={() => {
+					setSearch(false);
+					setAccess(false);
+					navigate("/cart");
+				}}
+			/>
 		</div>
 	);
 };
