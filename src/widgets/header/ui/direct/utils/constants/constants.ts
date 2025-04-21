64dab9773132d888224e244b7eb4e6cf8300cefd
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-21 16:08:30 +0300

    fix

--- src/widgets/header/ui/direct/utils/constants/constants.ts
+++ src/widgets/header/ui/direct/utils/constants/constants.ts
@@ -3,11 +3,14 @@ import { ImageType } from "@/shared/utils/types";
 export const images: ImageType[] = [
 	{
 		title: "search",
+		name: "header__search",
 	},
 	{
 		title: "user",
+		name: "header__user",
 	},
 	{
 		title: "shopping-cart",
+		name: "header__cart",
 	},
 ];
