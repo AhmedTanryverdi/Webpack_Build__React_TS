c8f22bf2381f3cb91c6e070d0d8eb16277575dce
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-22 22:56:27 +0300

    added cart slice

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,8 +1,10 @@
 import productSlice from "@/entities/model/slices/products/products";
+import cartSlice from "@/entities/model/slices/cart/cart";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
 	reducer: {
-        products: productSlice
-    },
+		products: productSlice,
+		cart: cartSlice,
+	},
 });
