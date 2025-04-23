ea3d44a89c793b9ce45c7b8654d6e48a096deb7e
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-23 23:05:15 +0300

    added pagination reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,10 +1,12 @@
 import productSlice from "@/entities/model/slices/products/products";
 import cartSlice from "@/entities/model/slices/cart/cart";
+import paginationSlice from "@/entities/model/slices/pagination/pagination";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
 	reducer: {
 		products: productSlice,
 		cart: cartSlice,
+		pagination: paginationSlice,
 	},
 });
