1d11e063e26d60cadd67224c7c6c0aad1bf8b213
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-23 22:35:56 +0300

    fix

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./button.scss";
 
-export const Button: React.FC<{ text: string; name: string }> = ({
+export const Button: React.FC<{ text: string; name: string; handleClick?: ()=>void }> = ({
 	text,
 	name,
+	handleClick,
 }): React.JSX.Element => {
-	return <button className={`button ${name}`}>{text}</button>;
+	return (
+		<button className={`button ${name}`} onClick={handleClick}>
+			{text}
+		</button>
+	);
 };
