efca279e2ef190423e90ccf8fd1fab28da61803b
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-24 14:32:42 +0300

    added layout to cart component

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { CartProductType, RootState } from "@/shared/utils/types";
 import { Table } from "./ui/table/Table";
+import { Button } from "@/shared/components/button/Button";
 
 export const Cart: React.FC = (): React.JSX.Element => {
 	const products = useSelector<RootState, CartProductType[]>(
@@ -30,7 +31,40 @@ export const Cart: React.FC = (): React.JSX.Element => {
 
 			<div className="container">
 				<div className="cart__content">
-					<Table products={products}/>
+					<Table products={products} />
+					<div className="result">
+						<div className="result__btns">
+							<Button
+								text="CONTINUE SHOPPING"
+								name="result__btns_btn"
+							/>
+							<Button
+								text="CLEAR SHOPPING CART"
+								name="result__btns_btn"
+							/>
+						</div>
+						<div className="result__counter">
+							<ul className="result__counter_items">
+								<li className="item">
+									<span className="item-name">Sub Total</span>
+									<span className="item-price">$106.97</span>
+									<hr />
+								</li>
+								<li className="item">
+									<span className="item-name">Shipping</span>
+									<span className="item-price">30</span>
+									<hr />
+								</li>
+								<li className="item">
+									<span className="item-name">Total</span>
+									<span className="item-price">$136.97</span>
+									<hr />
+								</li>
+							</ul>
+
+							<Button text="CHECHOUT" name="result__counter_btn" />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
