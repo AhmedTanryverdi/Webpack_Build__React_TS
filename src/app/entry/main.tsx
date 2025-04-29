40fb308fb8686200835b1c41d43124a86572ff38
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-29 14:01:28 +0300

    included react-query

--- src/app/entry/main.tsx
+++ src/app/entry/main.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import {store} from "@/app/store/index";
+import { store } from "@/app/store/index";
 import { App } from "./ui/App";
 import "../styles/app.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Provider store={store}>
-				<App />
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
 			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>
